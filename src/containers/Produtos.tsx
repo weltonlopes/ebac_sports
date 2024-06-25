87ed14c97f7f431dc2bfc0473f1f768e003d7acf
Welton Pereira <welton.lps@hotmail.com>
2024-06-25 05:15:00 -0300

    fim

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,38 +1,29 @@
-import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { Produto as ProdutoType } from '../App'
+import { useGetProdutosQuery } from '../services/api'
 
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
-
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+const ProdutosComponent = () => {
+  const { isLoading, data: produtos } = useGetProdutosQuery()
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
-    const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
+    const IdsDosFavoritos = produtos?.map((p) => p.id)
 
-    return IdsDosFavoritos.includes(produtoId)
+    if (IdsDosFavoritos?.includes(produto.id)) {
+      return true
+    } else return false
   }
+
+  if (isLoading) return <h2>Carregando...</h2>
 
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
           />
         ))}
       </S.Produtos>
